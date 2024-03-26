e7c6de6c89c90726a5f727cf6ea471f10afac840
Krzysztof (Christopher) <labiod@wp.pl>
2024-03-26 06:51:29 +0100

    [0.1.0-beta.3] - Move script enigine loader to static field in TemplateClass to load only once and work outside library

--- lib/src/main/kotlin/io/github/newagewriter/template/TemplateClass.kt
+++ lib/src/main/kotlin/io/github/newagewriter/template/TemplateClass.kt
@@ -37,7 +37,8 @@ class TemplateClass(
                 }
 
                 is Map<*, *> -> {
-                    val pattern = Regex("@foreach\\(\\\$$key as ([a-zA-Z0-1]+)\\s*->\\s*([a-zA-Z0-1]+)(, separator=\"([^\"]+)\")?\\):([^@]+)@end")
+                    val pattern =
+                        Regex("@foreach\\(\\\$$key as ([a-zA-Z0-1]+)\\s*->\\s*([a-zA-Z0-1]+)(, separator=\"([^\"]+)\")?\\):([^@]+)@end")
                     var matches = pattern.find(result)
                     var index = 0
                     if (key == "mapperList") {
@@ -62,6 +63,7 @@ class TemplateClass(
                         matches = matches.next()
                     }
                 }
+
                 else -> {
                     val pattern = Regex("\\\$\\{$key}")
                     result = result.replace(pattern, value.toString())
@@ -69,38 +71,55 @@ class TemplateClass(
             }
         }
 //        if (result.contains("GeneratedMapperFactory")) {
-            val ifPattern = Regex("#if\\(([^#]+)\\):([^#]+)#else ([^#]+)#endif")
-            var condMatches = ifPattern.find(result)
-            var diff = 0
-            val scriptManager = ScriptEngineManager()
-            val engine = scriptManager.getEngineByName("kotlin")
-        val bindings : Bindings = SimpleBindings()
+        val ifPattern = Regex("#if\\(([^#]+)\\):([^#]+)#else ([^#]+)#endif")
+        var condMatches = ifPattern.find(result)
+        var diff = 0
+        val engine = scriptManager.getEngineByName("kotlin")
+        val bindings: Bindings = SimpleBindings()
         bindings.putAll(varMap)
-            while (condMatches != null) {
-                condMatches.let { m ->
-                    val condition = m.groups[1]
-                    val resultOne = m.groups[2]
-                    val resultElse = m.groups[3]
+        while (condMatches != null) {
+            condMatches.let { m ->
+                val condition = m.groups[1]
+                val resultOne = m.groups[2]
+                val resultElse = m.groups[3]
 
-                    try {
-                        println("contitiond to execute: ${condition?.value}")
+                try {
+                    println("contitiond to execute: ${condition?.value}")
 
-                        val value = engine.eval(condition?.value, bindings) as Boolean
-                        val subStr = result.substring(m.range.first - diff, m.range.last + 1 - diff)
-                        val conditionResult = if (value) resultOne?.value else resultElse?.value
-                        diff += subStr.length - (conditionResult ?: "").length
-                        result = result.replace(subStr, conditionResult ?: "")
-                    } catch (ex: Exception) {
-                        println("problem with eval: $ex, $ex.pr")
-                        ex.printStackTrace(System.out)
-                    }
+                    val value = engine.eval(condition?.value, bindings) as Boolean
+                    val subStr = result.substring(m.range.first - diff, m.range.last + 1 - diff)
+                    val conditionResult = if (value) resultOne?.value else resultElse?.value
+                    diff += subStr.length - (conditionResult ?: "").length
+                    result = result.replace(subStr, conditionResult ?: "")
+                } catch (ex: Exception) {
+                    println("problem with eval: $ex, $ex.pr")
+                    ex.printStackTrace(System.out)
                 }
-                condMatches = condMatches.next()
+            }
+            condMatches = condMatches.next()
 //            }
         }
 //        println("code to compile: $result")
 //        val evv = engine.eval(result, bindings)
 //        println("result: ${evv}")
         return result
+    }
+
+    companion object {
+        private val scriptManager = ScriptEngineManager()
+
+        init {
+            scriptManager.getEngineByName("kotlin")?.let { engine ->
+                try {
+                    val myString = "test"
+                    val value = engine.eval("\"com.st\" == \"\"")
+                    println("result: $value")
+                    println("multi conditions: ${engine.eval("\"$myString\" == \"test\" && $value == false")}")
+//            ProcessorLogger.logD("[KB]", "condition: ${value}")
+                } catch (ex: Exception) {
+//            ProcessorLogger.logD("[KB]", "exception: $ex")
+                }
+            }
+        }
     }
 }
