9a1a5174432d134adcf88c07307d4d801b427f60
Krzysztof (Christopher) <labiod@wp.pl>
2024-04-06 13:51:31 +0200

    Sensitive data - remove from history.

---
+++ CHANGELOG.md
@@ -0,0 +1,34 @@
+# 0.1.0-beta.5
+
+### Features
+* Allow use #if command inside #foreach
+
+### Changes
+* change #end to #endforeach
+* Rearange CHANGLOG file (from newest to oldest)
+
+# 0.1.0-beta.4
+
+### Fixes
+* Fix script engine to works outside library. Add support for working in preprocessor
+
+### Changes
+* change @foreach to #foreach to unify commands
+* Update README file
+
+# 0.1.0-beta.3
+
+### Fixes
+* Fix script engine to works outside library. Still not working in preprocessor
+
+0.1.0-beta.2
+
+### Fixes
+* Fix #if #else pragma now working with two variable format: ${variable} or $variable.
+
+# 0.1.0-beta.1
+
+### Feature
+* Prepare first version of library
+* support for #if #else pragma
+* support for @foreach command that ends with @end
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,8 +1,11 @@
 import java.net.URI
 
 plugins {
+    java
     kotlin("jvm") version "1.8.0"
     `maven-publish`
+    signing
+    id("org.jetbrains.kotlin.kapt")
 }
 
 group = "io.github.newagewriter"
@@ -11,7 +14,7 @@ version = project.extra["lib_version"] as String
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = "org.gradle.sample"
+            groupId = "io.github.newagewriter"
             artifactId = "kt-generator"
             version = project.extra["lib_version"] as String
 
@@ -38,8 +41,8 @@ publishing {
                     }
                 }
                 scm {
-                    connection = "scm:git:git://example.com/my-library.git"
-                    developerConnection = "git@github.com:newagewriter/kt-generator.git"
+                    connection = "scm:git:git@github.com:newagewriter/kt-generator.git"
+                    developerConnection = "scm:git:ssh://git@github.com:newagewriter/kt-generator.git"
                     url = "https://github.com/newagewriter/kt-generator"
                 }
             }
@@ -73,9 +76,15 @@ repositories {
     mavenCentral()
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
 dependencies {
     implementation(kotlin("stdlib"))
     runtimeOnly("org.jetbrains.kotlin:kotlin-scripting-jsr223:1.8.0")
+//    kapt("org.jetbrains.kotlin:kotlin-scripting-jsr223:1.8.0")
     testImplementation(kotlin("test"))
 }
 
@@ -85,4 +94,9 @@ tasks.test {
 
 kotlin {
     jvmToolchain(8)
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["maven"])
 }
